f5c038a9854480dfea79c7f6166a6b258f2d3502
Saad Sabahuddin <saadmdsabahuddin@gmail.com>
2024-10-21 15:34:45 +0530

    Adding temp function

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
+function temp(){
+    console.log("Will remove this function");
+}
+
 app.listen(3000, ()=>console.log("Server is running"))
