016d8dccfd151c6446c4c0b72b30ef2276c887bc
Saad Sabahuddin <saadmdsabahuddin@gmail.com>
2024-10-21 15:35:27 +0530

    Removing temp function

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 const express = require('express');
 const app = express();
 
-function temp(){
-    console.log("Will remove this function");
-}
-
 app.listen(3000, ()=>console.log("Server is running"))
